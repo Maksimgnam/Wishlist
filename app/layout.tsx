b10e4297e2f34971523bb7ee64cf5cfea0d37db4
Maksgnap <107640396+Maksimgnam@users.noreply.github.com>
2024-08-23 17:42:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Coolday",
-  description: "Coolday",
+  title: "Wishlist",
+  description: "Wishlist",
 };
 
 export default function RootLayout({
